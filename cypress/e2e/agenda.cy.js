3a1b306cbde1e72a95b1dffb44dab1ed2a50a90a
Tomás Fava <tomsfava@gmail.com>
2025-04-13 12:37:05 -0300

    testes para a agenda de contatos

---
+++ cypress/e2e/agenda.cy.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visit('https://agenda-contatos-react.vercel.app/')
+    cy.get('.sc-beqWaB.eQdhbg.contato').its('length').as('qtde')
+    cy.get('.sc-eDDNvR.cTVgex').first().find('li').first()
+    .invoke('text').its('length').as('tmn')
+  })
+
+  it('Testa a inclusão de um contato', () => {
+    cy.get('input[type="text"]').type('meuNome')
+    cy.get('input[type="email"]').type('meuemail@test.com')
+    cy.get('input[type="tel"]').type('11 975477172')
+    cy.get('.adicionar').click()
+    cy.get('@qtde').then((qtde) => {
+      cy.get('.sc-beqWaB.eQdhbg.contato').should('have.length', qtde + 1)
+    })
+  })
+  
+  it('Testa a edição de um contato', () => {
+    cy.get('.edit').first().click()
+    cy.get('input[type="text"]').type('a')
+    cy.get('.alterar').click()
+    cy.get('@tmn').then((tmn) => {
+      cy.get('.sc-eDDNvR.cTVgex').first().find('li').first()
+      .invoke('text').should('have.length', tmn + 1)
+    })
+  })
+
+  it('Testa a remoção de um contato', () => {
+    cy.get('.delete').last().click()
+    cy.get('@qtde').then((qtde) => {
+      cy.get('.sc-beqWaB.eQdhbg.contato').should('have.length', qtde - 1)
+    })
+  })
+})
